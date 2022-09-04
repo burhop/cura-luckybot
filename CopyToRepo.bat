19ead09efd26be01b1eeb2837870433d53adfe45
burhop <mark.burhop@gmail.com>
2022-09-04 13:13:28 -0400

    Initial working version

---
+++ CopyToRepo.bat
@@ -0,0 +1,9 @@
+REM If you are on Windows, copy this batch file to a place outside your git repo and modify with your own paths.
+
+copy /y "C:\Program Files\Ultimaker Cura 5.1.0\share\cura\resources\materials\chocolate.xml.fdm_material" "D:\development\repos\cura-luckybot\cura5.x\share\resources\materials\chocolate.xml.fdm_material"
+copy /y "C:\Program Files\Ultimaker Cura 5.1.0\share\cura\resources\definitions\creality_ender3pro_luckybot.def.json" "D:\development\repos\cura-luckybot\cura5.x\share\resources\definitions\creality_ender3pro_luckybot.def.json"
+copy /y "C:\Program Files\Ultimaker Cura 5.1.0\share\cura\resources\extruders\creality_base_extruder_0_luckybot.def.json" "D:\development\repos\cura-luckybot\cura5.x\share\resources\extruders\creality_base_extruder_0_luckybot.def.json"
+copy /y "C:\Program Files\Ultimaker Cura 5.1.0\share\cura\resources\meshes\creality_ender3_luckybot.3mf" "D:\development\repos\cura-luckybot\cura5.x\share\resources\meshes\creality_ender3_luckybot.3mf"
+copy /y "C:\Program Files\Ultimaker Cura 5.1.0\share\cura\resources\quality\creality\base\base_global_standard_luckybot.inst.cfg" "D:\development\repos\cura-luckybot\cura5.x\share\resources\quality\creality\base\base_global_standard_luckybot.inst.cfg" 
+copy /y "C:\Program Files\Ultimaker Cura 5.1.0\share\cura\resources\variants\creality_ender3pro_luckybot1.0.inst.cfg" "D:\development\repos\cura-luckybot\cura5.x\share\resources\variants\creality_ender3pro_luckybot1.0.inst.cfg"
+pause
